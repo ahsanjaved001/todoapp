606968d238da04c7664a5128e807ad4038eb35f6
Ahsan Javed <ahsanjvd001@gmail.com>
2021-10-20 17:58:22 +0500

    Infrastructure Updated

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 let mongoose, sequelize;
 
@@ -7,24 +6,7 @@ if (process.env.DATABASE_DRIVER === 'mongoose')
 else
   sequelize = require('./models/mysql/connection');
 
-//const sequelize = require('./sq_db');
 const app = require('./app');
-
-// const DB = process.env.DATABASE.replace(
-//   '<password>',
-//   process.env.DATABASE_PASSWORD
-// );
-
-// // sequelize.sync().then(result => {
-// //   console.log('Mysql connection');
-// // }).catch(err => console.log(err));
-
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
--- sq_db.js
+++
@@ -1,8 +0,0 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('todoapp', 'root', '6155276@Carbonteq', {
-//     dialect: 'mysql',
-//     host: 'localhost'
-// });
-
-// module.exports = sequelize;
